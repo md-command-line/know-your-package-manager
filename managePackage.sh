dbecf310ff774b8778d7e1e28a58b4d4edf07f77
michael dimmitt <michaelgdimmitt@gmail.com>
2019-04-04 16:27:45 -0400

    helper-function-runs-correct-package-manager

---
+++ managePackage.sh
@@ -0,0 +1,24 @@
+checkLockFiles(){
+  result=$(echo "$1" | wc -l);
+  if [ "$result" -ge "2" ]; then
+    echo 'two or more lock files detected ... please delete one to continue';
+  elif [ "$1" == "pack" ]; then
+    which npm;
+  elif [ "$1" == "yarn" ]; then
+    which yarn;
+  elif [ "$(ls | grep lock | cut -c 1-4 | wc -l)" -eq "0" ]; then
+    echo " echo 'no lock files found'";
+  else
+    which $1
+  fi;
+};
+
+helper(){
+  source ./verifyEnvironment.sh
+  lockFilesFound=$(ls | grep lock | cut -c 1-4 | grep "pack\|yarn")
+
+  packageManager=$(checkLockFiles "$(echo "$lockFilesFound")" npm)
+  $packageManager $*;
+};
+
+helper $1
