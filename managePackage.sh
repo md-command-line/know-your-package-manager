cafb05731a9222f6d3aeffc4c5da57762e96cea3
michael dimmitt <michaelgdimmitt@gmail.com>
2019-04-04 21:58:56 -0400

    expose the usecase two or more lock files detected

--- managePackage.sh
+++ managePackage.sh
@@ -7,7 +7,7 @@ checkLockFiles(){
   elif [ "$1" == "yarn" ]; then
     which yarn;
   elif [ "$(ls | grep lock | cut -c 1-4 | wc -l)" -eq "0" ]; then
-    echo "echo 'no lock file found'"
+    echo 'no lock file found'
   else
     which $1
   fi;
@@ -15,7 +15,6 @@ checkLockFiles(){
 
 helper(){
   lockFilesFound=$(ls | grep lock | cut -c 1-4 | grep "pack\|yarn")
-
   echo "$(checkLockFiles "$(echo "$lockFilesFound")") $*";
 };
 
@@ -27,11 +26,16 @@ dispatch(){
     $(which $pM) $H $*
     # here we do not display a message ... other libraries may be using normal npm.
     # this package aims to ensure there are no issues with normal package management operations.
-  elif [ "$(echo "$(helper))" | cut -c 7-13)" = "no lock" ]; then
+  elif [ "$(echo "$(helper))" | cut -c 1-7)" = "no lock" ]; then
    $(which $pM) $* $H
    echo ;
    echo "you just experienced $pM"
    echo 'because: no lock file found'
+  elif [ "$(echo "$(helper))" | cut -c 1-31)" = "two or more lock files detected" ]; then
+   $(which $pM) $* $H
+   echo ;
+   echo "you just experienced $pM"
+   echo 'because: two or more lock files found'
  else
    $(helper) $H $*
    echo ;
